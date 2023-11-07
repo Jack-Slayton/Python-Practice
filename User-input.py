f74398ac4d4405571df92e32d841dfcb30822df2
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2023-11-07 20:46:11 +0000

    Finished Project 1: User-input

---
+++ User-input.py
@@ -0,0 +1,5 @@
+response = input("What is your name? ")
+response2 = input("What is your age? ")
+year = 2023
+var1 = (100 - int(response2)) + int(year)
+print(str(response) + " will be turning 100 in the year " + str(var1))
