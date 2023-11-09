0f9ed337c6870fe89a0562a66c06996565e34af6
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2023-11-09 16:34:09 +0000

    Finished project 5: password-generator

--- Password-generator.py
+++ Password-generator.py
@@ -2,16 +2,15 @@ import random
 
 response = input("How strong of a password do you need? 1 = weak 2 = strong. ")
 weakpass = ["12345","Password","password123","123","password"]
-lestpass = ["9", "30", "21", "17", "12", "22", "15"]
-alp = [qwertyuiopasdfghjklzxcvbnm]
+length = 21
+alp = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456778990!@#$%^&*()"
 
 if response == "1":
     var1 = "weak"
     password = random.choice(weakpass)
 elif response == "2":
     var1 = "strong"
-    lenpass = int(random.choice(lestpass))
-
+    password = "".join(random.sample(alp,length))
 else:
     print("Not a acceptable response.")
-print("Your " + var1 + " password is: " + password + ".")
+print("Your " + var1 + " password is: " + password)
