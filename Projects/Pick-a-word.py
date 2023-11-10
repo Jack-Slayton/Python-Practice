077adfd749d1fba86a67598bd3c05eb0727a7c79
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2023-11-10 17:46:43 +0000

    progress

--- Projects/Pick-a-word.py
+++ Projects/Pick-a-word.py
@@ -1,10 +1,29 @@
 import random
 
 with open('/workspaces/Python-Practice/Projects/SOWPODS.txt', 'r') as f:
-  random_lines = random.choice(open("/workspaces/Python-Practice/Projects/SOWPODS.txt").readlines())
-print(random_lines)
-var1 = len(random_lines)
-print(var1)
+  word = random.choice(open("/workspaces/Python-Practice/Projects/SOWPODS.txt").readlines())
+var1 = len(word)
+print(word)
+print("Welcome to Hangman:")
+print(" ______")
+print("/     |")
+print("|     |")
+print("|     ")
+print("|     ")
+print("|    ")
+print("|      ")
+print("|      ")
+print( )
+spaces = "________________________________"
+print(spaces[0:var1])
+
+oneg = input("What is your first guess? ")
+
+if oneg in word:
+  print("Correct!")
+
+
+
 print("HANGMAN")
 print(" ______")
 print("/     |")
