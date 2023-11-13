5f0ac806d06c38c9b4347592d2edbaf29d42f78a
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2023-11-13 21:06:51 +0000

    Working on find letter

--- Projects/Find-letter.py
+++ Projects/Find-letter.py
@@ -1,20 +1,17 @@
 word = "GATOLATOUS"
 var1 = len(word)
 
+livesleft = str("6")
 guess = str.capitalize(input("What is your first guess? "))
 
 spaces = "____________________"
 spaced = spaces[0:var1]
-print(spaced)
 lis = list(spaced)
-print(lis)
-
 
 var2 = 0
 var3 = 1
 for i in range(var1):
     if guess in word[var2:var3]:
-        print(var3)
         lis[var3 - 1] = guess
         spaced = "".join(lis)
         var2 = var2 + 1
@@ -24,3 +21,32 @@ for i in range(var1):
         var3 = var3 + 1
 
 print(spaced)
+if spaced == spaces[0:var1]:
+    print("one layer")
+    livesleft = livesleft - 1
+else:
+    print("Corect")
+print("You have " + livesleft + " lives left.")
+spaced1 = spaced
+
+lis = list(spaced)
+guess = str.capitalize(input("What is your guess? "))
+
+var2 = 0
+var3 = 1
+for i in range(var1):
+    if guess in word[var2:var3]:
+        print(var3)
+        lis[var3 - 1] = guess
+        spaced1 = "".join(lis)
+        var2 = var2 + 1
+        var3 = var3 + 1
+    else:
+        var2 = var2 + 1
+        var3 = var3 + 1
+
+print(spaced)
+if spaced1 == spaced:
+    print("second layer")
+else:
+    print("Corect")
