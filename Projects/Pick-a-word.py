476fefa982efaaa29db1b07b46b92eedce9a886e
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2023-11-13 15:04:14 +0000

    Made lives in hangman

--- Projects/Pick-a-word.py
+++ Projects/Pick-a-word.py
@@ -2,6 +2,7 @@ import random
 
 with open('/workspaces/Python-Practice/Projects/SOWPODS.txt', 'r') as f:
   word = random.choice(open("/workspaces/Python-Practice/Projects/SOWPODS.txt").readlines())
+life = 0
 var1 = len(word)
 print(word)
 print("Welcome to Hangman:")
@@ -19,12 +20,14 @@ print(spaces[0:var1])
 
 oneg = input("What is your first guess? ")
 
-if oneg in word:
+if str.capitalize(oneg) in word:
   print("Correct!")
+else:
+  print("Wrong!")
+  life = int(life) + 1
+  print(life)
 
 
-
-print("HANGMAN")
 print(" ______")
 print("/     |")
 print("|     |")
