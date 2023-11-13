1e50fb8182c7b025ced3315450e16c64c31642dd
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2023-11-13 20:47:36 +0000

    Fixed Find-letter.py AGAIN and created test.py

--- Projects/Find-letter.py
+++ Projects/Find-letter.py
@@ -9,18 +9,18 @@ print(spaced)
 lis = list(spaced)
 print(lis)
 
+
 var2 = 0
 var3 = 1
-for x in word:
+for i in range(var1):
     if guess in word[var2:var3]:
         print(var3)
-        lis[var3] = guess
+        lis[var3 - 1] = guess
+        spaced = "".join(lis)
         var2 = var2 + 1
         var3 = var3 + 1
     else:
         var2 = var2 + 1
         var3 = var3 + 1
 
-print(lis)
-spaced = "".join(lis)
 print(spaced)
---
+++ Projects/test.py
@@ -0,0 +1,5 @@
+#currently using to test FOR loop for hangman
+var1 = 0
+for i in range(50):
+    var1 = var1 + 1
+    print(var1)
